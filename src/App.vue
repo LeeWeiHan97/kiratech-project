41f1eff93175a6c0e3c8ba62e7814c2f48b476c7
LeeWeiHan97 <weihanlee88@gmail.com>
2022-12-10 22:06:29 +0800

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view/>
 </template>
 
 <style>
+body {
+  background-color: rgb(224, 224, 224);
+  overflow: hidden;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #575757;
+  font-size: 16px;
+  width: 100%;
+  display: flex;
+	justify-content: center;
 }
 
 nav {
@@ -26,5 +31,117 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.mainDiv {
+  position: absolute;
+  margin-top: 30px;
+  margin-bottom: 30px;
+  width: 90vw;
+}
+
+@media (min-width: 600px) {
+	.mainDiv {
+		width: 50vw;
+	}
+}
+
+@media (min-width: 992px) {
+	.mainDiv {
+		width: 36vw;
+	}
+}
+
+.titleDiv {
+  font-size: 1.5em;
+  font-weight: bold;
+}
+
+.searchDiv {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 10px;
+  margin-top: 25px;
+}
+
+.searchBar {
+  width: 100%;
+  margin-right: 15px;
+  padding: 8px 12px;
+  border-radius: 4px;
+  border: 1px solid grey;
+}
+
+.refreshIcon {
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+}
+
+.userListDiv {
+  display: flex;
+  flex-direction: column;
+  padding: 15px 10px;
+}
+
+.userDiv {
+	background-color: rgb(236, 232, 232);
+	border: 2px solid rgb(177, 177, 177);
+	border-radius: 6px;
+	display: flex;
+	padding: 10px 12px;
+	margin-top: 15px;
+	font-size: 25px;
+  cursor: pointer;
+  transition: all 0.35s;
+}
+
+.userDiv:active,
+.userDiv:hover {
+    border: 2px solid rgb(51, 51, 51)
+}
+
+.userDetailsDiv {
+  padding: 10px 5px;
+}
+
+.userImage {
+  width: 100px;
+	height: 100px;
+	object-fit: cover;
+	margin-right: 10px;
+	background-color: white;
+	border: 1px solid rgb(209, 207, 207);
+}
+
+.userSpan {
+  font-weight: bold;
+}
+
+.userSubDiv {
+  width: 100%;
+  font-size: 0.95rem;
+	line-height: 1.3em;
+  text-align: left;
+}
+
+.overflowDiv {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 280px;
+}
+
+@media (min-width: 600px) {
+	.overflowDiv {
+    max-width: 300px;
+  }
+}
+
+@media (min-width: 992px) {
+	.overflowDiv {
+    max-width: 400px;
+  }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,105 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+	import axios from 'axios'
+	import RefreshIcon from '../assets/refresh.png'
+	import { computed } from "vue";
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+
+	export default {
+		name: 'HomeView',
+
+		data() {
+			return {
+				originalUserList: [],
+				userList: [],
+				search: ""
+			}
+		},
+
+		methods: {
+			getUser() {
+				axios
+					.get('https://randomuser.me/api/?results=20')
+					.then(async response => {
+						console.log(response.data.results)
+						this.originalUserList = computed(() => response.data.results)
+						this.userList = computed(() => response.data.results)
+					})
+			},
+
+			filterUserList(search) {
+				this.search = computed(() => search)
+
+				// if user removes search, return the original fetched list
+				if (!search || search.trim() === "" || search === null) {
+					this.userList = computed(() => this.originalUserList)
+					return
+				}
+
+				let filteredList = []
+				for (var user of this.userList) {
+					if (user.name.first.toLowerCase().includes(search) || user.name.last.toLowerCase().includes(search)) {
+						filteredList.push(user)
+					}
+				}
+
+				this.userList = computed(() => filteredList)
+			}
+		},
+
+		beforeMount() {
+			this.getUser()
+		},
+
+		render() {
+			return (
+				<div class="mainDiv">
+					<div class="titleDiv">
+						Users
+					</div>
+
+					<div class="searchDiv">
+						<input class="searchBar" value={this.search} placeholder="Search for users here..." onInput={(e)=>{
+							this.filterUserList(e.target.value)
+						}} />
+
+						<img src={RefreshIcon} class="refreshIcon" onClick={()=>{
+							this.getUser()
+						}} />
+					</div>
+
+					<div class="userListDiv">
+						{
+							this.userList && this.userList.length > 0 ?
+
+							this.userList.map((user) => {
+								return (
+									<div class="userDiv" onClick={()=>{}}>
+										<img class="userImage" src={user.picture.medium} alt={user.name.first} />
+										
+										<div class="userDetailsDiv">
+											<div class="userSubDiv">
+												<span class="userSpan">Name:</span> {user.name.title} {user.name.first} {user.name.last}
+											</div>
+
+											<div class="userSubDiv">
+												<span class="userSpan">Gender:</span> {user.gender}
+											</div>
+
+											<div class="userSubDiv">
+												<span class="userSpan">Country:</span> {user.location.country}
+											</div>
+
+											<div class="userSubDiv overflowDiv">
+												<span class="userSpan">Email:</span> {user.email}
+											</div>
+										</div>
+									</div>
+								)
+							}) : null
+						}
+					</div>
+				</div>
+			)
+		}
+	}
 </script>
